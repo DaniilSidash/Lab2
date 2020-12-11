140604b650219e1787990e1f9dc2dcd5ec97b81e
DaniilSidash <70960703+DaniilSidash@users.noreply.github.com>
2020-12-11 12:38:57 +0300

    Add files via upload

---
+++ individual.py
@@ -0,0 +1,7 @@
+from math import sqrt
+a = float(input("1-ое число"))
+b = float(input("2-ое число"))
+
+print("Среднее арифметическое=", ((a+b) / 2))
+print("Среднее геометрическое", sqrt(a * b))
+
